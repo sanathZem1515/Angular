2da5115fc052f469b4c90041af58aedc7852f649
sanathZem1515 <sanath.bajjuri@zemosolabs.com>
2023-02-26 02:05:57 +0530

    22 & 23 styles, styleUrls,different selectors usage [app-server] .app-server

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -2,12 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  // templateUrl: './servers.component.html',
-  template: `
-    <app-server></app-server>
-    <app-server></app-server>
-    <app-server></app-server>
-  `,
+  // selector:'[app-servers]', <div app-server></div>
+  // selector:'.app-server, <div class="app-server"></div>
+  templateUrl: './servers.component.html',
+  // template: `
+  //   <app-server></app-server>
+  //   <app-server></app-server>
+  //   <app-server></app-server>
+  // `,
   styleUrls: ['./servers.component.css'],
 })
 export class ServersComponent {}
