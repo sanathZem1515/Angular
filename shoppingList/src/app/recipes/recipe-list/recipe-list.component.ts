504f4c6c89ea5d69d737bfe4e958b74c3a3bfb20
sanathZem1515 <sanath.bajjuri@zemosolabs.com>
2023-02-27 01:13:57 +0530

    53 recipe list

--- shoppingList/src/app/recipes/recipe-list/recipe-list.component.ts
+++ shoppingList/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
-export class RecipeListComponent {
-  recipes = []
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is test',
+      'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.eatingwell.com%2Frecipe%2F7949991%2Fcucumber-sandwich%2F&psig=AOvVaw2vOj-keoqXsK1UlrJIFByQ&ust=1677526658866000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCLCW6Pr3s_0CFQAAAAAdAAAAABAD'
+    ),
+  ];
 
+  constructor() {}
+
+  ngOnInit() {}
 }
