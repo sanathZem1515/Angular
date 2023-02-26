93e834688148f9f79a53c217138605c7d95115c1
sanathZem1515 <sanath.bajjuri@zemosolabs.com>
2023-02-26 23:45:28 +0530

    37 ngIf

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -16,6 +16,7 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = "No server was created!";
   serverName="TestServer"
+  serverCreated=false;
 
   constructor() {
     setTimeout(() => {
@@ -27,6 +28,7 @@ export class ServersComponent implements OnInit {
 
   onCreateServer() {
     this.serverCreationStatus = "Server was created is "+this.serverName;
+    this.serverCreated = true
   }
 
   onUpdateServerName(event:Event) {
