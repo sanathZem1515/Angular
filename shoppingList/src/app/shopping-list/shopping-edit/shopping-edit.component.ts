e34ab8746f91e9b91ebd8f5aafb97de2385ce4cb
sanathZem1515 <sanath.bajjuri@zemosolabs.com>
2023-02-28 19:31:11 +0530

    89 Child to Parent and Parent to Child in SHopping List

--- shoppingList/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ shoppingList/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
   styleUrls: ['./shopping-edit.component.css']
 })
-export class ShoppingEditComponent {
+export class ShoppingEditComponent implements OnInit{
+
+  @ViewChild('nameInput') nameInputRef : ElementRef;
+  @ViewChild('amountInput') amountInputRef : ElementRef;
+
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+      
+  }
+
+  onAdd() {
+    this.ingredientAdded.emit(new Ingredient(this.nameInputRef.nativeElement.value,this.amountInputRef.nativeElement.value));
+  }
+
+  onDelete() {
+
+  }
+
+  onClear() {
+    // this.name = ""
+    // this.amount = undefined
+  }
 
 }
--- shoppingList/src/app/shopping-list/shopping-list.component.ts
+++ shoppingList/src/app/shopping-list/shopping-list.component.ts
@@ -20,4 +20,8 @@ export class ShoppingListComponent implements OnInit{
 
   }
 
+  onAdded(ingredient:Ingredient) {
+    this.ingredients.push(ingredient);
+  }
+
 }
