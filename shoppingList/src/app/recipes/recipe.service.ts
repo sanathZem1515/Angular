d14893c5a8dbc2701a3111f6f433a6499ff011d9
sanathZem1515 <sanath.bajjuri@zemosolabs.com>
2023-03-01 21:38:38 +0530

    121 Adding Ingredinets to Recipe

--- shoppingList/src/app/recipes/recipe.service.ts
+++ shoppingList/src/app/recipes/recipe.service.ts
@@ -1,19 +1,22 @@
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 import { Recipe } from './recipe.model';
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
-  
+
   private recipes: Recipe[] = [
     new Recipe(
       'A Test Recipe',
       'This is test',
-      'https://images.immediate.co.uk/production/volatile/sites/30/2022/10/Campfire-stew-afe40d7.jpg'
+      'https://images.immediate.co.uk/production/volatile/sites/30/2022/10/Campfire-stew-afe40d7.jpg',
+      [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]
     ),
     new Recipe(
       'A Test Recipe 2',
       'This is test 2',
-      'https://images.immediate.co.uk/production/volatile/sites/30/2022/10/Campfire-stew-afe40d7.jpg'
+      'https://images.immediate.co.uk/production/volatile/sites/30/2022/10/Campfire-stew-afe40d7.jpg',
+      [(new Ingredient('Buns', 2), new Ingredient('Meat', 1))]
     ),
   ];
 
