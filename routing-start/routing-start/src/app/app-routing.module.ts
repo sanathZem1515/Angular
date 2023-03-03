275980c265228ce9476c4efaace6827b903d2e5d
sanathZem1515 <sanath.bajjuri@zemosolabs.com>
2023-03-04 01:50:39 +0530

    150 passing static data using route

--- routing-start/routing-start/src/app/app-routing.module.ts
+++ routing-start/routing-start/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { AuthGuard } from "./auth-guard.service";
+import { ErrorPageComponent } from "./error-page/error-page.component";
 import { HomeComponent } from "./home/home.component";
 import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
 import { CanDeactivateGuard } from "./servers/edit-server/can-deactivate-guard.service";
@@ -20,14 +21,19 @@ const appRoutes: Routes = [
   {
     path: 'servers',
     // canActivate:[AuthGuard],
-    canActivateChild:[AuthGuard],
+    canActivateChild: [AuthGuard],
     component: ServersComponent,
     children: [
       { path: ':id', component: ServerComponent },
-      { path: ':id/edit', component: EditServerComponent, canDeactivate:[CanDeactivateGuard] },
+      {
+        path: ':id/edit',
+        component: EditServerComponent,
+        canDeactivate: [CanDeactivateGuard],
+      },
     ],
   },
-  { path: 'not-found', component: PageNotFoundComponent },
+//   { path: 'not-found', component: PageNotFoundComponent },
+  { path: 'not-found', component: ErrorPageComponent, data:{message:'Page not found !'}},
   { path: '**', redirectTo: '/not-found' },
 ];
 
