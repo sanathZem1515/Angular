555ca8c845654245df7227f169851967968f6f0b
sanathZem1515 <sanath.bajjuri@zemosolabs.com>
2023-03-04 01:27:51 +0530

    149 Can Deactivate

--- routing-start/routing-start/src/app/app.module.ts
+++ routing-start/routing-start/src/app/app.module.ts
@@ -15,6 +15,7 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 import { AppRoutingModule } from './app-routing.module';
 import { AuthGuard } from './auth-guard.service';
 import { AuthService } from './auth.service';
+import { CanDeactivateGuard } from './servers/edit-server/can-deactivate-guard.service';
 
 
 @NgModule({
@@ -29,7 +30,7 @@ import { AuthService } from './auth.service';
     PageNotFoundComponent,
   ],
   imports: [BrowserModule, FormsModule, AppRoutingModule],
-  providers: [ServersService,AuthGuard,AuthService],
+  providers: [ServersService,AuthGuard,AuthService,CanDeactivateGuard],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- routing-start/routing-start/src/app/servers/edit-server/edit-server.component.ts
+++ routing-start/routing-start/src/app/servers/edit-server/edit-server.component.ts
@@ -1,32 +1,34 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Observable } from 'rxjs';
 
 import { ServersService } from '../servers.service';
+import { CanComponentDeactivate } from './can-deactivate-guard.service';
 
 @Component({
   selector: 'app-edit-server',
   templateUrl: './edit-server.component.html',
   styleUrls: ['./edit-server.component.css'],
 })
-export class EditServerComponent implements OnInit {
+export class EditServerComponent implements OnInit, CanComponentDeactivate{
   server: { id: number; name: string; status: string };
   serverName = '';
   serverStatus = '';
   allowEdit = false;
+  changesSaved = false;
 
   constructor(
     private serversService: ServersService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private router: Router
   ) {}
 
   ngOnInit() {
     console.log(this.route.snapshot.queryParams);
     console.log(this.route.snapshot.fragment);
-    this.route.queryParams.subscribe(
-      (queryParams : Params) => {
-        this.allowEdit = queryParams['allowEdit'] === '1' ? true : false;
-      }
-    );
+    this.route.queryParams.subscribe((queryParams: Params) => {
+      this.allowEdit = queryParams['allowEdit'] === '1' ? true : false;
+    });
     this.route.fragment.subscribe();
     this.server = this.serversService.getServer(1);
     this.serverName = this.server.name;
@@ -38,5 +40,18 @@ export class EditServerComponent implements OnInit {
       name: this.serverName,
       status: this.serverStatus,
     });
+    this.changesSaved = true;
+    this.router.navigate(['../'], { relativeTo: this.route });
+  }
+
+  canDeactivate() : boolean | Observable<boolean> | Promise<boolean> {
+    if(!this.allowEdit) {
+      return true;
+    }
+    if((this.serverName !== this. server.name || this.serverStatus !== this.server.status) && !this.changesSaved) {
+      return confirm('Do you want to discard the changes ?');
+    } else {
+      return true;
+    }
   }
 }
