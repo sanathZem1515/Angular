dcd826d3dce8a5844a5ecff6c5416c46537b7041
sanathZem1515 <sanath.bajjuri@zemosolabs.com>
2023-03-08 15:47:04 +0530

    208 FormGroup in FormGroup

--- forms-reactive-start/forms-reactive-start/src/app/app.component.ts
+++ forms-reactive-start/forms-reactive-start/src/app/app.component.ts
@@ -4,18 +4,20 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
   genders = ['male', 'female'];
-  signupForm :FormGroup;
+  signupForm: FormGroup;
 
   ngOnInit() {
     this.signupForm = new FormGroup({
-      'username':new FormControl(null,Validators.required),
-      'email': new FormControl(null,[Validators.required,Validators.email]),
-      'gender':new FormControl('male'),
-    })
+      userData: new FormGroup({
+        username: new FormControl(null, Validators.required),
+        email: new FormControl(null, [Validators.required, Validators.email]),
+      }),
+      gender: new FormControl('male'),
+    });
   }
 
   onSubmit() {
