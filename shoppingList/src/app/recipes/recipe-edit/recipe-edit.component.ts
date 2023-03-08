7a8dd9832c1176b30dad2c14ffc2cea5f6aae431
sanathZem1515 <sanath.bajjuri@zemosolabs.com>
2023-03-08 23:52:40 +0530

    238 Deleting Ingredients

--- shoppingList/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ shoppingList/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -55,6 +55,10 @@ export class RecipeEditComponent implements OnInit {
     this.router.navigate(['../'], { relativeTo: this.route });
   }
 
+  onDeleteIngredient(index : number) {
+    (<FormArray>this.recipeForm.get('ingredients')).removeAt(index);
+  }
+
   getControls() {
     return (<FormArray>this.recipeForm.get('ingredients')).controls;
   }
