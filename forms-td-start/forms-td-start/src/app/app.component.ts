c8dbfed4f705ea14aa3e2542678409e2b38f7990
sanathZem1515 <sanath.bajjuri@zemosolabs.com>
2023-03-08 11:19:18 +0530

    189 View Child

--- forms-td-start/forms-td-start/src/app/app.component.ts
+++ forms-td-start/forms-td-start/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  @ViewChild('f') signUpForm:NgForm
+
   suggestUserName() {
     const suggestedName = 'Superuser';
+  }
+
+  onSubmit() {
+    console.log(this.signUpForm);
   }
 }
