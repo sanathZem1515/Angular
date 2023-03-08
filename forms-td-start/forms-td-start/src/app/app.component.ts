38985ffbf4bb6ee021a2870d172aa2d965d55e1b
sanathZem1515 <sanath.bajjuri@zemosolabs.com>
2023-03-08 12:12:26 +0530

    193 and 194 Default value

--- forms-td-start/forms-td-start/src/app/app.component.ts
+++ forms-td-start/forms-td-start/src/app/app.component.ts
@@ -9,6 +9,7 @@ import { NgForm } from '@angular/forms';
 export class AppComponent {
 
   @ViewChild('f') signUpForm:NgForm
+  defaultValue='pet';
 
   suggestUserName() {
     const suggestedName = 'Superuser';
