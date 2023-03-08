59fbc04d55eeece1f2770aba8505784345d48790
sanathZem1515 <sanath.bajjuri@zemosolabs.com>
2023-03-08 13:01:52 +0530

    199 and 200 Using form Data and reset

--- forms-td-start/forms-td-start/src/app/app.component.ts
+++ forms-td-start/forms-td-start/src/app/app.component.ts
@@ -11,7 +11,15 @@ export class AppComponent {
   defaultValue = 'pet';
   answer = '';
   genders = ['Male', 'Female'];
+  user = {
+    username:'',
+    email:'',
+    secretQuestion:'',
+    answer:'',
+    gender:''
+  }
 
+  submitted = false;
   suggestUserName() {
     const suggestedName = 'Superuser';
     // this.signUpForm.setValue({
@@ -28,9 +36,18 @@ export class AppComponent {
         username:suggestedName
       }
     })
+
   }
 
   onSubmit() {
     console.log(this.signUpForm);
+    this.submitted = true;
+    this.user.username = this.signUpForm.value.userData.username;
+    this.user.email = this.signUpForm.value.userData.email;
+    this.user.secretQuestion = this.signUpForm.value.secret;
+    this.user.answer = this.signUpForm.value.questionAnswer;
+    this.user.gender = this.signUpForm.value.gender;
+
+    this.signUpForm.reset();
   }
 }
