be4d681604b2e27268a26e214577e4b5317e5dbc
sanathZem1515 <sanath.bajjuri@zemosolabs.com>
2023-03-09 12:28:57 +0530

    256 Getting Data

--- http-01-start/http-01-start/src/app/app.component.ts
+++ http-01-start/http-01-start/src/app/app.component.ts
@@ -11,7 +11,9 @@ export class AppComponent implements OnInit {
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.fetchPosts();
+  }
 
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
@@ -22,16 +24,24 @@ export class AppComponent implements OnInit {
     ).subscribe(
       (responseData) => {
         console.log(responseData);
-        
       }
     );
   }
 
   onFetchPosts() {
     // Send Http request
+    this.fetchPosts();
   }
 
   onClearPosts() {
     // Send Http request
+  }
+
+  private fetchPosts() {
+    this.http.get('https://np-complete-guide-38063-default-rtdb.firebaseio.com/posts.json').subscribe(
+      (posts) => {
+        console.log(posts);
+      }
+    );
   }
 }
