0454fb5ea53836d939730db08dab7ba363a30063
sanathZem1515 <sanath.bajjuri@zemosolabs.com>
2023-03-09 16:28:08 +0530

    270 Observing different types of responses

--- http-01-start/http-01-start/src/app/posts.service.ts
+++ http-01-start/http-01-start/src/app/posts.service.ts
@@ -1,7 +1,12 @@
-import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import {
+  HttpClient,
+  HttpEventType,
+  HttpHeaders,
+  HttpParams,
+} from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Subject, throwError } from 'rxjs';
-import { catchError, map } from 'rxjs/operators';
+import { catchError, map, tap } from 'rxjs/operators';
 import { Post } from './post.model';
 
 @Injectable({
@@ -18,43 +23,63 @@ export class PostsService {
   createAndStorePost(title: string, content: string) {
     const postData: Post = { title: title, content: content };
 
-    this.http.post<{ name: string }>(this.url, postData).subscribe(
-      (responseData) => {
-        console.log(responseData);
-      },
-      (error) => {
-        this.error.next(error.message);
-      }
-    );
+    this.http
+      .post<{ name: string }>(this.url, postData, {
+        observe: 'response',
+      })
+      .subscribe(
+        (responseData) => {
+          console.log(responseData);
+        },
+        (error) => {
+          this.error.next(error.message);
+        }
+      );
   }
 
   fetchPosts() {
-
     let searchParams = new HttpParams();
-    searchParams = searchParams.append('print','pretty');
+    searchParams = searchParams.append('print', 'pretty');
     searchParams = searchParams.append('custom', 'key');
 
-    return this.http.get<{ [key: string]: Post }>(this.url,{
-      headers:new HttpHeaders({"Custom-Header":"Hello"}),
-      // params:new HttpParams().set('print','pretty')
-      params:searchParams,
-    }).pipe(
-      map((responseData) => {
-        const postArray: Post[] = [];
-        for (const key in responseData) {
-          if (responseData.hasOwnProperty(key)) {
-            postArray.push({ ...responseData[key], id: key });
-          }
-        }
-        return postArray;
-      }),
-      catchError((errorRes) => {
-        return throwError(errorRes);
+    return this.http
+      .get<{ [key: string]: Post }>(this.url, {
+        headers: new HttpHeaders({ 'Custom-Header': 'Hello' }),
+        // params:new HttpParams().set('print','pretty')
+        params: searchParams,
       })
-    );
+      .pipe(
+        map((responseData) => {
+          const postArray: Post[] = [];
+          for (const key in responseData) {
+            if (responseData.hasOwnProperty(key)) {
+              postArray.push({ ...responseData[key], id: key });
+            }
+          }
+          return postArray;
+        }),
+        catchError((errorRes) => {
+          return throwError(errorRes);
+        })
+      );
   }
 
   deletePosts() {
-    return this.http.delete(this.url);
+    return this.http
+      .delete(this.url, {
+        observe: 'events',
+      })
+      .pipe(
+        tap((event) => {
+          console.log(event);
+
+          if (event.type === HttpEventType.Sent) {
+            console.log(event.type);
+          }
+          if (event.type === HttpEventType.Response) {
+            console.log(event.body);
+          }
+        })
+      );
   }
 }
