a4292dc72f8893b40314d06c54812d6ca573c643
sanathZem1515 <sanath.bajjuri@zemosolabs.com>
2023-03-09 12:26:35 +0530

    255 Sending a POST Request

--- http-01-start/http-01-start/src/app/app.component.ts
+++ http-01-start/http-01-start/src/app/app.component.ts
@@ -16,6 +16,15 @@ export class AppComponent implements OnInit {
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
     console.log(postData);
+
+    this.http.post(
+      'https://np-complete-guide-38063-default-rtdb.firebaseio.com/posts.json',postData
+    ).subscribe(
+      (responseData) => {
+        console.log(responseData);
+        
+      }
+    );
   }
 
   onFetchPosts() {
