e387cdf084a25b74164ea2a78d8ae0722418b64a
sanathZem1515 <sanath.bajjuri@zemosolabs.com>
2023-03-09 17:56:53 +0530

    275 Multiple Interceptors

--- http-01-start/http-01-start/src/app/auth-interceptor.service.ts
+++ http-01-start/http-01-start/src/app/auth-interceptor.service.ts
@@ -14,17 +14,11 @@ export class AuthInterceptorService implements HttpInterceptor {
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
     console.log('Request is on its way');
+    console.log(req.url);
 
     const modifyRequest = req.clone({
       headers: req.headers.append('Auth', 'xyz'),
     });
-    return next.handle(modifyRequest).pipe(
-      tap((event) => {
-        if (event.type === HttpEventType.Response) {
-          console.log('Response arrived, body data: ');
-          console.log(event.body);
-        }
-      })
-    );
+    return next.handle(modifyRequest);
   }
 }
