2418bda03e2553b37f5eeb62e370d765eece3bfd
sanathZem1515 <sanath.bajjuri@zemosolabs.com>
2023-03-09 13:03:55 +0530

    257 Using rxjs operators to transform data

--- http-01-start/http-01-start/src/app/app.component.ts
+++ http-01-start/http-01-start/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
   loadedPosts = [];
@@ -19,13 +20,14 @@ export class AppComponent implements OnInit {
     // Send Http request
     console.log(postData);
 
-    this.http.post(
-      'https://np-complete-guide-38063-default-rtdb.firebaseio.com/posts.json',postData
-    ).subscribe(
-      (responseData) => {
+    this.http
+      .post(
+        'https://np-complete-guide-38063-default-rtdb.firebaseio.com/posts.json',
+        postData
+      )
+      .subscribe((responseData) => {
         console.log(responseData);
-      }
-    );
+      });
   }
 
   onFetchPosts() {
@@ -38,10 +40,23 @@ export class AppComponent implements OnInit {
   }
 
   private fetchPosts() {
-    this.http.get('https://np-complete-guide-38063-default-rtdb.firebaseio.com/posts.json').subscribe(
-      (posts) => {
+    this.http
+      .get(
+        'https://np-complete-guide-38063-default-rtdb.firebaseio.com/posts.json'
+      )
+      .pipe(
+        map((responseData) => {
+          const postArray = [];
+          for (const key in responseData) {
+            if (responseData.hasOwnProperty(key)) {
+              postArray.push({ ...responseData[key], id: key });
+            }
+          }
+          return postArray;
+        })
+      )
+      .subscribe((posts) => {
         console.log(posts);
-      }
-    );
+      });
   }
 }
