0d800291a59c0144b5f84fa207f3531b933af901
sanathZem1515 <sanath.bajjuri@zemosolabs.com>
2023-03-09 19:47:20 +0530

    281 Storing Recipes

--- shoppingList/src/app/shared/data-storage.service.ts
+++ shoppingList/src/app/shared/data-storage.service.ts
@@ -1,12 +1,20 @@
-import { HttpClient } from "@angular/common/http";
-import { Injectable, OnInit } from "@angular/core";
+import { HttpClient } from '@angular/common/http';
+import { Injectable, OnInit } from '@angular/core';
+import { Recipe } from '../recipes/recipe.model';
+import { RecipeService } from '../recipes/recipe.service';
 
-@Injectable({providedIn:'root'})
-export class DataStorageService implements OnInit{
-    constructor(private http:HttpClient) {
-        
-    }
-    ngOnInit(): void {
-        throw new Error("Method not implemented.");
-    }
+@Injectable({ providedIn: 'root' })
+export class DataStorageService implements OnInit {
+  constructor(private http: HttpClient, private recipeService: RecipeService) {}
+
+  url : string = 'https://shopping-list-65b39-default-rtdb.firebaseio.com/recipes.json';
+
+  ngOnInit(): void {}
+
+  storeRecipes() {
+    const recipes = this.recipeService.getRecipes();
+    return this.http.put(this.url,recipes).subscribe((responseData)=>{
+        console.log(responseData);
+    });
+  }
 }
