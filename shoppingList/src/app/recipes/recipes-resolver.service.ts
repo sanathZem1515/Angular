a9a0caef59a996191a47abe6967236852e5e81e7
sanathZem1515 <sanath.bajjuri@zemosolabs.com>
2023-03-09 20:34:50 +0530

    285 Fixing a bug with resolver

--- shoppingList/src/app/recipes/recipes-resolver.service.ts
+++ shoppingList/src/app/recipes/recipes-resolver.service.ts
@@ -1,15 +1,31 @@
-import { Injectable } from "@angular/core";
-import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from "@angular/router";
-import { Observable } from "rxjs";
-import { DataStorageService } from "../shared/data-storage.service";
-import { Recipe } from "./recipe.model";
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  Resolve,
+  RouterStateSnapshot,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { DataStorageService } from '../shared/data-storage.service';
+import { Recipe } from './recipe.model';
+import { RecipeService } from './recipe.service';
 
-@Injectable({providedIn:'root'})
+@Injectable({ providedIn: 'root' })
 export class RecipesResolverService implements Resolve<Recipe[]> {
+  constructor(
+    private dataStorageService: DataStorageService,
+    private recipeService: RecipeService
+  ) {}
 
-    constructor(private dataStorageService : DataStorageService) {}
+  resolve(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Recipe[] | Observable<Recipe[]> | Promise<Recipe[]> {
+    const recipes = this.recipeService.getRecipes();
 
-    resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Recipe[] | Observable<Recipe[]> | Promise<Recipe[]> {
-        return this.dataStorageService.fetchRecipes();
+    if (recipes.length === 0) {
+      return this.dataStorageService.fetchRecipes();
+    } else {
+      return recipes;
     }
+  }
 }
