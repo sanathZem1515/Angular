0fe45c8d38a9122e86c01942e173be172ce5d9f6
sanathZem1515 <sanath.bajjuri@zemosolabs.com>
2023-03-09 16:21:42 +0530

    269 Adding query parameters

--- http-01-start/http-01-start/src/app/posts.service.ts
+++ http-01-start/http-01-start/src/app/posts.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Subject, throwError } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
@@ -29,8 +29,15 @@ export class PostsService {
   }
 
   fetchPosts() {
+
+    let searchParams = new HttpParams();
+    searchParams = searchParams.append('print','pretty');
+    searchParams = searchParams.append('custom', 'key');
+
     return this.http.get<{ [key: string]: Post }>(this.url,{
-      headers:new HttpHeaders({"Custom-Header":"Hello"})
+      headers:new HttpHeaders({"Custom-Header":"Hello"}),
+      // params:new HttpParams().set('print','pretty')
+      params:searchParams,
     }).pipe(
       map((responseData) => {
         const postArray: Post[] = [];
