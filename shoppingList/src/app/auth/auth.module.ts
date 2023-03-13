84404f85f7302ef5b7efbef6a82fdc633388e6a5
sanathZem1515 <sanath.bajjuri@zemosolabs.com>
2023-03-13 15:15:38 +0530

    329 Adding an auth feature module

---
+++ shoppingList/src/app/auth/auth.module.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { SharedModule } from '../shared/shared.module';
+import { AuthRoutingModule } from './auth-routing.module';
+import { AuthComponent } from './auth.component';
+
+@NgModule({
+  declarations: [AuthComponent],
+  imports: [FormsModule, AuthRoutingModule, SharedModule],
+})
+export class AuthModule {}
