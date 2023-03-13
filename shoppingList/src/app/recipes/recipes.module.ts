1d5453f671dfbd1cde08ad8ac105c5192f0bfec4
sanathZem1515 <sanath.bajjuri@zemosolabs.com>
2023-03-13 13:25:56 +0530

    326 Shopping list Feature module

--- shoppingList/src/app/recipes/recipes.module.ts
+++ shoppingList/src/app/recipes/recipes.module.ts
@@ -1,14 +1,14 @@
-import { CommonModule } from "@angular/common";
-import { NgModule } from "@angular/core";
-import { ReactiveFormsModule } from "@angular/forms";
-import { RouterModule } from "@angular/router";
-import { RecipeDetailComponent } from "./recipe-detail/recipe-detail.component";
-import { RecipeEditComponent } from "./recipe-edit/recipe-edit.component";
-import { RecipeItemComponent } from "./recipe-list/recipe-item/recipe-item.component";
-import { RecipeListComponent } from "./recipe-list/recipe-list.component";
-import { RecipeStartComponent } from "./recipe-start/recipe-start.component";
-import { RecipesRoutingModule } from "./recipes-routing.module";
-import { RecipesComponent } from "./recipes.component";
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
+import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
+import { RecipeItemComponent } from './recipe-list/recipe-item/recipe-item.component';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
+import { RecipeStartComponent } from './recipe-start/recipe-start.component';
+import { RecipesRoutingModule } from './recipes-routing.module';
+import { RecipesComponent } from './recipes.component';
 
 @NgModule({
   declarations: [
@@ -19,11 +19,11 @@ import { RecipesComponent } from "./recipes.component";
     RecipeStartComponent,
     RecipeEditComponent,
   ],
-  imports:[
+  imports: [
     RouterModule,
     CommonModule,
     ReactiveFormsModule,
-    RecipesRoutingModule
-  ]
+    RecipesRoutingModule,
+  ],
 })
 export class RecipesModule {}
