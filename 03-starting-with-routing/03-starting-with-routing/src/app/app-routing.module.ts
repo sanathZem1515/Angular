832396f2c6999d10851755cc52d6f31d582b1988
sanathZem1515 <sanath.bajjuri@zemosolabs.com>
2023-03-14 12:13:33 +0530

    352 Lazy Loading

--- 03-starting-with-routing/03-starting-with-routing/src/app/app-routing.module.ts
+++ 03-starting-with-routing/03-starting-with-routing/src/app/app-routing.module.ts
@@ -1,23 +1,25 @@
-import { NgModule } from '@angular/core';
-import { Route, RouterModule } from '@angular/router';
-import { AboutComponent } from './about/about.component';
+import { NgModule } from "@angular/core";
+import { Route, RouterModule } from "@angular/router";
+import { AboutComponent } from "./about/about.component";
 
-import { WelcomeComponent } from './welcome/welcome.component';
+import { WelcomeComponent } from "./welcome/welcome.component";
 
 const routes: Route[] = [
   {
-    path: '',
+    path: "",
     component: WelcomeComponent,
   },
   {
-    path: 'about',
-    component: AboutComponent,
+    path: "about",
+    // component: AboutComponent,
+    loadComponent: () =>
+      import("./about/about.component").then((m) => m.AboutComponent),
   },
   {
-    path: 'dashboard',
+    path: "dashboard",
     loadChildren: () =>
-      import('./dashboard/dashboard-routing.module').then(
-        (mod) => mod.DashboardRoutingModule
+      import("./dashboard/routes").then(
+        (mod) => mod.DASHBOARD_ROUTES
       ),
   },
 ];
