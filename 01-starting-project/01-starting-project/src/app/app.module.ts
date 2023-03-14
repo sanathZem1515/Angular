f473fee3ed010e6a8e5b8dfbb845ae4032730f2c
sanathZem1515 <sanath.bajjuri@zemosolabs.com>
2023-03-14 11:18:16 +0530

    348 Migrating an other component

--- 01-starting-project/01-starting-project/src/app/app.module.ts
+++ 01-starting-project/01-starting-project/src/app/app.module.ts
@@ -1,13 +1,13 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppComponent } from './app.component';
-import { DetailsComponent } from './welcome/details/details.component';
-import { WelcomeComponent } from './welcome/welcome.component';
+import { AppComponent } from "./app.component";
+import { DetailsComponent } from "./welcome/details/details.component";
+import { WelcomeComponent } from "./welcome/welcome.component";
 
 @NgModule({
-  declarations: [AppComponent, WelcomeComponent],
-  imports: [BrowserModule, DetailsComponent],
+  declarations: [AppComponent],
+  imports: [BrowserModule, WelcomeComponent],
   providers: [],
   bootstrap: [AppComponent],
 })
