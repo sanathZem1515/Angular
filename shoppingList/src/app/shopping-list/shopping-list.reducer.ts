1953322b0b08213f34430d0a23488ed1e3e6c797
sanathZem1515 <sanath.bajjuri@zemosolabs.com>
2023-03-17 00:42:47 +0530

    359 Adding Logic to the Reducer

--- shoppingList/src/app/shopping-list/shopping-list.reducer.ts
+++ shoppingList/src/app/shopping-list/shopping-list.reducer.ts
@@ -1,11 +1,16 @@
+import { Action } from '@ngrx/store';
 import { Ingredient } from '../shared/ingredient.model';
 
 const initialState = {
   ingredients: [new Ingredient('Apples', 5), new Ingredient('Tomatoes', 10)],
 };
 
-export function ShoppingListReducer(state = initialState, action) {
-
-
-    
+export function ShoppingListReducer(state = initialState, action: Action) {
+  switch (action.type) {
+    case 'ADD_INGREDIENT':
+      return {
+        ...state,
+        ingredients: [...state.ingredients, action],
+      };
+  }
 }
