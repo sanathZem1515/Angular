5a82096778c408e1e5ff1a1baae1716f7b051eb5
sanathZem1515 <sanath.bajjuri@zemosolabs.com>
2023-03-17 11:47:12 +0530

    371 Setting up Auth Reducer and actions

--- shoppingList/src/app/auth/store/auth.reducer.ts
+++ shoppingList/src/app/auth/store/auth.reducer.ts
@@ -1,15 +1,38 @@
-import { Action } from "@ngrx/store";
-import { User } from "../user.model";
+import { User } from '../user.model';
+import * as AuthActions from './auth.actions';
 
 export interface AuthReducerType {
-    user:User
+  user: User;
 }
 
-
-const initialState : AuthReducerType = {
+const initialState: AuthReducerType = {
   user: null,
 };
 
-export function authReducer (state = initialState,actions:Action) {
-    return state;
+export function authReducer(
+  state = initialState,
+  action: AuthActions.AuthActions
+) {
+  switch (action.type) {
+    case AuthActions.LOGIN:
+      const user = new User(
+        action.payload.email,
+        action.payload.userId,
+        action.payload.token,
+        action.payload.expirationDate
+      );
+
+      return {
+        ...state,
+        user: user,
+      };
+
+    case AuthActions.LOGOUT:
+      return {
+        ...state,
+        user: null,
+      };
+    default:
+      return state;
+  }
 }
