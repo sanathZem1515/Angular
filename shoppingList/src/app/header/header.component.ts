fcd2856b413e0c81971a6490a4241a048afc53d3
sanathZem1515 <sanath.bajjuri@zemosolabs.com>
2023-03-17 12:10:53 +0530

    374 Naming of Actions

--- shoppingList/src/app/header/header.component.ts
+++ shoppingList/src/app/header/header.component.ts
@@ -28,7 +28,6 @@ export class HeaderComponent implements OnInit, OnDestroy {
   ) {}
 
   ngOnInit(): void {
-
     this.userSub = this.store
       .select('auth')
       .pipe(map((authState) => authState.user))
